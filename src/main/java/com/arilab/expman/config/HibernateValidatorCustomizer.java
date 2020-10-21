581893c54ee1165ede17bb3dd1dddaf0200d55c3
Kosmas Deligkaris <deligkarisk@gmail.com>
2020-10-21 11:28:58 +0900

    [EX-5] Replaced hibernate's validator with the one managed by Spring

--- src/main/java/com/arilab/expman/config/HibernateValidatorCustomizer.java
+++ src/main/java/com/arilab/expman/config/HibernateValidatorCustomizer.java
@@ -1,25 +1,31 @@
 package com.arilab.expman.config;
 
-import org.springframework.beans.factory.ObjectProvider;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.stereotype.Component;
 
-import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.util.Map;
+
 
 @Component
 public class HibernateValidatorCustomizer implements HibernatePropertiesCustomizer {
 
-    private final ObjectProvider<Validator> provider;
-
-    public HibernateValidatorCustomizer(ObjectProvider<javax.validation.Validator> provider) {
-        this.provider=provider;
-    }
+    @Autowired
+    private ValidatorFactory validatorFactory;
 
     public void customize(Map<String, Object> hibernateProperties) {
-        Validator validator = provider.getIfUnique();
-        if (validator != null) {
-            hibernateProperties.put("javax.persistence.validation.factory", validator);
+        if (validatorFactory != null) {
+            hibernateProperties.put("javax.persistence.validation.factory", validatorFactory);
         }
     }
 }
+
+
+
+
+
+
+
+
