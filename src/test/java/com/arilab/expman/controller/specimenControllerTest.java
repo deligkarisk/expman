a845c31a3304222cf38ff11653a79c16771aceea
Kosmas Deligkaris <deligkarisk@gmail.com>
2021-01-28 13:54:19 +0900

    hsql db works but there are errors in the table creations.

--- src/test/java/com/arilab/expman/controller/specimenControllerTest.java
+++ src/test/java/com/arilab/expman/controller/specimenControllerTest.java
@@ -16,6 +16,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
