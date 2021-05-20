b0069bf99f40c3b936398601c832a4f67f822255
Kosmas Deligkaris <deligkarisk@gmail.com>
2021-05-20 14:26:24 +0900

    Added test for editspecimen view failing when basis of record is null.

--- src/test/java/com/arilab/expman/frontend/editSpecimenHTMLTest.java
+++ src/test/java/com/arilab/expman/frontend/editSpecimenHTMLTest.java
@@ -1,19 +1,60 @@
 package com.arilab.expman.frontend;
 
+import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class editSpecimenHTMLTest {
 
+    @Value("${REFERENCE_SPECIMEN_CODE_NULLBASISOFRECORD}")
+    private String REFERENCE_SPECIMEN_CODE;
+
+
+    @Autowired
+    private WebApplicationContext wac;
+
+
+    private  WebClient webClient;
+
+
+    @Before
+    public void setup() {
+        webClient = MockMvcWebClientBuilder
+                .webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void editSpecimenPageWithNullBasisOfRecordWorks() throws IOException {
+        HtmlPage  editSpecimenPage = webClient.getPage("https://localhost/edit/specimen/" + REFERENCE_SPECIMEN_CODE);
+        assertEquals(200, editSpecimenPage.getWebResponse().getStatusCode());
+    }
 
 
 
 
 /*
 
-    HtmlPage createMsgFormPage = webClient.getPage("https://localhost/edit/specimen/" + REFERENCE_SPECIMEN_CODE);
 
     HtmlForm form = createMsgFormPage.getHtmlElementById("editSpecimenForm");
     HtmlSelect basisOfRecordSelect = form.getSelectByName("basisOfRecord");
